10087ab50b7e02ff2c4df7b0cb3f9bff51c8fc17
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-19 19:23:45 +0600

    add MongoDB connection and create index view for URL shortener

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://shakoyatsujon:3W8IdD5g6kON8AJW@cluster0.0p8evwy.mongodb.net/",
+    {
+      dbName: "Nodejs101db",
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
 const PORT = 1000;
 
 app.listen(PORT, () => {
